aff309a43088f50b7d879da7e8577df2953ce2b8
folkert <folkert@6e73ccbf-f98f-4734-8601-04fa8acba097>
2009-06-06 11:46:46 +0000

    *** empty log message ***
    git-svn-id: http://localhost/svn/entropybroker/trunk@3 6e73ccbf-f98f-4734-8601-04fa8acba097

--- pool.cpp
+++ pool.cpp
@@ -1,51 +1,44 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <openssl/blowfish.h>
 #include <openssl/sha.h>
 #include <string.h>
 #include <math.h>
+#include <unistd.h>
 
 #include "pool.h"
-
-#define min(x, y)       ((x)<(y)?(x):(y))
+#include "error.h"
+#include "kernel_prng_io.h"
+#include "math.h"
+#include "utils.h"
 
 pool::pool()
 {
 	bits_in_pool = 0;
+
+	if (kernel_rng_read_non_blocking(entropy_pool, sizeof(entropy_pool)) == -1)
+		error_exit("failed reading entropy data to kernel RNG");
+}
+
+pool::pool(char *state_file)
+{
+	int fd = open(state_file, O_RDWR);
+	if (fd == -1)
+		error_exit("error opening %s", state_file);
+
+	if (READ(fd, (char *)entropy_pool, sizeof(entropy_pool)) != sizeof(entropy_pool))
+		error_exit("file %s does not contain required %d bytes", state_file, sizeof(entropy_pool));
+
+	close(fd);
 }
 
 pool::~pool()
 {
+	if (kernel_rng_write_non_blocking(entropy_pool, sizeof(entropy_pool)) == -1)
+		error_exit("failed writing entropy data to kernel RNG");
+
 	memset(entropy_pool, 0x00, sizeof(entropy_pool));
-}
-
-int pool::determine_number_of_bits_of_data(unsigned char *data, int n_bytes)
-{
-	int cnts[256], loop;
-	double ent=0.0;
-
-	memset(cnts, 0x00, sizeof(cnts));
-
-	for(loop=0; loop<n_bytes; loop++)
-	{
-		cnts[data[loop]]++;
-	}
-
-	for(loop=0; loop<256;loop++)
-	{
-		double prob = (double)cnts[loop] / (double)n_bytes;
-
-		if (prob > 0.0)
-		{
-			ent += prob * (log(1.0/prob)/log(2.0));
-		}
-	}
-
-	ent *= (double)n_bytes;
-
-	if (ent < 0.0) ent=0.0;
-
-	ent = min((double)(n_bytes*8), ent);
-
-	return ent;
 }
 
 int pool::add_entropy_data(unsigned char entropy_data[8])
@@ -74,27 +67,37 @@ int pool::get_n_bits_in_pool(void)
 	return bits_in_pool;
 }
 
-int pool::get_entropy_data(unsigned char entropy_data[8])
+int pool::get_entropy_data(unsigned char *entropy_data, int n_bytes_requested, char prng_ok)
 {
 	unsigned char temp_buffer[POOL_SIZE / 8];
 	unsigned char ivec[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
 	BF_KEY key;
-	int rc = 0;
+	int n_given;
 	unsigned char hash[SHA512_DIGEST_LENGTH];
 
-	if (bits_in_pool < 64)
-		rc = -1;
+	n_given = n_bytes_requested;
+	if (!prng_ok)
+		n_given = min(n_given, bits_in_pool / 8);
+	n_given = min(n_given, SHA512_DIGEST_LENGTH);
 
-	SHA512(entropy_pool, sizeof(entropy_pool), hash);
-	memcpy(entropy_data, hash, sizeof(entropy_data));
+	if (n_given > 0)
+	{
+		SHA512(entropy_pool, sizeof(entropy_pool), hash);
+		memcpy(entropy_data, hash, n_given);
 
-	bits_in_pool -= 64;
-	if (bits_in_pool < 0)
-		bits_in_pool = 0;
+		bits_in_pool -= (n_given * 8);
+		if (bits_in_pool < 0)
+			bits_in_pool = 0;
 
-	BF_set_key(&key, sizeof(hash), hash);
-	BF_cbc_encrypt(entropy_pool, temp_buffer, (POOL_SIZE / 8), &key, ivec, BF_DECRYPT);
-	memcpy(entropy_pool, temp_buffer, (POOL_SIZE / 8));
+		BF_set_key(&key, sizeof(hash), hash);
+		BF_cbc_encrypt(entropy_pool, temp_buffer, (POOL_SIZE / 8), &key, ivec, BF_DECRYPT);
+		memcpy(entropy_pool, temp_buffer, (POOL_SIZE / 8));
+	}
 
-	return rc;
+	return n_given;
+}
+
+int pool::get_pool_size(void)
+{
+	return POOL_SIZE;
 }
