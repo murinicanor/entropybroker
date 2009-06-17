9fd4ea5a6398ec30998c57b2d094fe25f0355dfa
root <root@6e73ccbf-f98f-4734-8601-04fa8acba097>
2009-06-17 17:13:23 +0000

    Initial revision
    git-svn-id: http://localhost/svn/entropybroker/trunk@14 6e73ccbf-f98f-4734-8601-04fa8acba097

---
+++ server_utils.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "error.h"
+
+void emit_buffer_to_file(char *file, unsigned char *data, int n_bytes)
+{
+	FILE *fh = fopen(file, "a+");
+	if (!fh)
+		error_exit("Error opening %s for append access", file);
+
+	if (fwrite(data, 1, n_bytes, fh) != n_bytes)
+		error_exit("Short write to %s", file);
+
+	fclose(fh);
+}
