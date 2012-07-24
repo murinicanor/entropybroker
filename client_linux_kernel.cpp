8b8116ec4ed26afc5f2dfcdec2034fa84716e80f
folkert <folkert@6e73ccbf-f98f-4734-8601-04fa8acba097>
2012-07-24 17:04:34 +0000

    correctly show new entropy count
    git-svn-id: http://localhost/svn/entropybroker/trunk@37 6e73ccbf-f98f-4734-8601-04fa8acba097

--- client_linux_kernel.cpp
+++ client_linux_kernel.cpp
@@ -260,9 +260,9 @@ int main(int argc, char *argv[])
 			{
 				int information_n_bits = determine_number_of_bits_of_data(buffer, will_get_n_bytes);
 
-				dolog(LOG_DEBUG, "%d bits from server contains %d bits of information, new entropy count: %d", will_get_n_bits, information_n_bits, kernel_rng_get_entropy_count());
-
 				rc = kernel_rng_add_entropy(buffer, will_get_n_bytes, information_n_bits);
+
+				dolog(LOG_DEBUG, "%d bits from server contains %d bits of information, new entropy count: %d", will_get_n_bits, information_n_bits, kernel_rng_get_entropy_count());
 			}
 
 			if (rc == -1)
