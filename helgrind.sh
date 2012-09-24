d22607686fa235be5d05f7897d9100708f826385
folkert <folkert@6e73ccbf-f98f-4734-8601-04fa8acba097>
2012-09-24 15:17:43 +0000

    debugging
    git-svn-id: http://localhost/svn/entropybroker/trunk@462 6e73ccbf-f98f-4734-8601-04fa8acba097

--- helgrind.sh
+++ helgrind.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-valgrind --tool=helgrind ./entropy_broker -c ./entropy_broker.conf -n 2> err8.log
+valgrind --tool=helgrind ./entropy_broker -c ./entropy_broker.conf -n -l err.log 2> err8.log
