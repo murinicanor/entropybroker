faa22a591c1a4c86d3dc14da2d2a45ba101d413a
folkert <folkert@6e73ccbf-f98f-4734-8601-04fa8acba097>
2012-09-26 08:49:15 +0000

    made configure script more robust / check for openssl as well
    git-svn-id: http://localhost/svn/entropybroker/trunk@494 6e73ccbf-f98f-4734-8601-04fa8acba097

---
+++ test_openssl.cpp
@@ -0,0 +1,15 @@
+#include <openssl/aes.h>
+#include <openssl/blowfish.h>
+#include <openssl/camellia.h>
+#include <openssl/des.h>
+#include <openssl/err.h>
+#include <openssl/md5.h>
+#include <openssl/rand.h>
+#include <openssl/ripemd.h>
+#include <openssl/sha.h>
+#include <openssl/whrlpool.h>
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
