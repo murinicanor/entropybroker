66aa758b746aa68b7cd77b1233f3aa2d8a4f13e7
folkert <folkert@6e73ccbf-f98f-4734-8601-04fa8acba097>
2012-11-15 10:04:52 +0000

    more white space in main menu (2)
    git-svn-id: http://localhost/svn/entropybroker/trunk@683 6e73ccbf-f98f-4734-8601-04fa8acba097

--- http_file_root.cpp
+++ http_file_root.cpp
@@ -32,9 +32,9 @@ http_bundle * http_file_root::do_request(http_request_t request_type, std::strin
 	return new http_bundle(reply_headers,
 			get_style_header() + 
 			"<TABLE CLASS=\"table2\" WIDTH=100%><TR><TD>\n"
-			"<A HREF=\"/stats.html\"><IMG ALIGN=\"middle\" SRC=\"statistics.png\" BORDER=0>stats</A><BR><BR>\n"
-			"<A HREF=\"/logfile.html\"><IMG ALIGN=\"middle\" SRC=\"logfiles.png\" BORDER=0>log file</A><BR><BR>\n"
-			"<A HREF=\"/version.html\"><IMG ALIGN=\"middle\" SRC=\"logo-bw.png\" BORDER=0>version</A>\n"
+			"<A HREF=\"/stats.html\"><IMG ALIGN=\"middle\" SRC=\"statistics.png\" BORDER=0> stats</A><BR><BR>\n"
+			"<A HREF=\"/logfile.html\"><IMG ALIGN=\"middle\" SRC=\"logfiles.png\" BORDER=0> log file</A><BR><BR>\n"
+			"<A HREF=\"/version.html\"><IMG ALIGN=\"middle\" SRC=\"logo-bw.png\" BORDER=0> version</A>\n"
 			"</TD></TR></TABLE>\n" +
 			get_style_tail());
 }
