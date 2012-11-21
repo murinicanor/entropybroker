c5b2f138e34019f6702e4d97be10cd0d86d87bc7
folkert <folkert@6e73ccbf-f98f-4734-8601-04fa8acba097>
2012-11-21 21:00:03 +0000

    html 2.0 -> html5 (2)
    git-svn-id: http://localhost/svn/entropybroker/trunk@742 6e73ccbf-f98f-4734-8601-04fa8acba097

--- http_file_stats.cpp
+++ http_file_stats.cpp
@@ -69,46 +69,46 @@ http_bundle * http_file_stats::do_request(http_request_t request_type, std::stri
 		{
 			statistics *pcs = p -> stats_user;
 
-			content += "<TABLE CLASS=\"table2\" WIDTH=\"100%\">\n";
-			content += std::string("<TR><TD WIDTH=\"200\">username:</TD><TD>") + p -> username + "</TD></TR>\n";
-			content += std::string("<TR><TD>host:</TD><TD>") + p -> host + "</TD></TR>\n";
-			content += std::string("<TR><TD>type:</TD><TD>") + p -> type + "</TD></TR>\n";
-			content += std::string("<TR><TD>is server:</TD><TD>") + (p -> is_server ? "yes" : "no") + "</TD></TR>\n";
+			content += "<table class=\"table2\" WIDTH=\"100%\">\n";
+			content += std::string("<tr><td class=\"keys\">username:</td><td>") + p -> username + "</td></tr>\n";
+			content += std::string("<tr><td>host:</td><td>") + p -> host + "</td></tr>\n";
+			content += std::string("<tr><td>type:</td><td>") + p -> type + "</td></tr>\n";
+			content += std::string("<tr><td>is server:</td><td>") + (p -> is_server ? "yes" : "no") + "</td></tr>\n";
 
-			content += "<TR><TD>connected since:</TD><TD>" + time_to_str((time_t)pcs -> get_since_ts()) + "</TD></TR>\n";
-			content += format("<TR><TD>duration:</TD><TD>%fs</TD></TR>\n", now - pcs -> get_since_ts());
-			content += format("<TR><TD>avg time between msgs:</TD><TD>%fs</TD></TR>\n", (now - pcs -> get_since_ts()) / double(pcs -> get_msg_cnt()));
-			content += "<TR><TD>last message:</TD><TD>" + time_to_str((time_t)pcs -> get_last_msg_ts()) + "</TD></TR>\n";
-			content += "<TR><TD>last put message:</TD><TD>" + time_to_str((time_t)pcs -> get_last_put_msg_ts()) + "</TD></TR>\n";
+			content += "<tr><td>connected since:</td><td>" + time_to_str((time_t)pcs -> get_since_ts()) + "</td></tr>\n";
+			content += format("<tr><td>duration:</td><td>%fs</td></tr>\n", now - pcs -> get_since_ts());
+			content += format("<tr><td>avg time between msgs:</td><td>%fs</td></tr>\n", (now - pcs -> get_since_ts()) / double(pcs -> get_msg_cnt()));
+			content += "<tr><td>last message:</td><td>" + time_to_str((time_t)pcs -> get_last_msg_ts()) + "</td></tr>\n";
+			content += "<tr><td>last put message:</td><td>" + time_to_str((time_t)pcs -> get_last_put_msg_ts()) + "</td></tr>\n";
 
-			content += format("<TR><TD>not allowed:</TD><TD>%d</TD></TR>\n", pcs -> get_times_not_allowed());
-			content += format("<TR><TD>quota:</TD><TD>%d</TD></TR>\n", pcs -> get_times_quota());
-			content += format("<TR><TD>pools empty:</TD><TD>%d</TD></TR>\n", pcs -> get_times_empty());
-			content += format("<TR><TD>full:</TD><TD>%d</TD></TR>\n", pcs -> get_times_full());
+			content += format("<tr><td>not allowed:</td><td>%d</td></tr>\n", pcs -> get_times_not_allowed());
+			content += format("<tr><td>quota:</td><td>%d</td></tr>\n", pcs -> get_times_quota());
+			content += format("<tr><td>pools empty:</td><td>%d</td></tr>\n", pcs -> get_times_empty());
+			content += format("<tr><td>full:</td><td>%d</td></tr>\n", pcs -> get_times_full());
 
 			long long int total_bits_recv = 0, total_bits_recv_in = 0;
 			int n_recv = 0;
 			pcs -> get_recvs(&total_bits_recv, &n_recv, &total_bits_recv_in);
-			content += format("<TR><TD>put requests:</TD><TD>%d</TD></TR>\n", n_recv);
-			content += format("<TR><TD>bits put:</TD><TD>%lld</TD></TR>\n", total_bits_recv);
-			content += format("<TR><TD>bits put density:</TD><TD>%f%%</TD></TR>\n", double(total_bits_recv * 100) / double(total_bits_recv_in));
-			content += format("<TR><TD>avg bits/put:</TD><TD>%f</TD></TR>\n", double(total_bits_recv) / double(n_recv));
-			content += format("<TR><TD>put bps:</TD><TD>%f</TD></TR>\n", double(total_bits_recv) / (now - pcs -> get_since_ts()));
+			content += format("<tr><td>put requests:</td><td>%d</td></tr>\n", n_recv);
+			content += format("<tr><td>bits put:</td><td>%lld</td></tr>\n", total_bits_recv);
+			content += format("<tr><td>bits put density:</td><td>%f%%</td></tr>\n", double(total_bits_recv * 100) / double(total_bits_recv_in));
+			content += format("<tr><td>avg bits/put:</td><td>%f</td></tr>\n", double(total_bits_recv) / double(n_recv));
+			content += format("<tr><td>put bps:</td><td>%f</td></tr>\n", double(total_bits_recv) / (now - pcs -> get_since_ts()));
 
 			long long int total_bits_sent = 0;
 			int n_sent = 0;
 			pcs -> get_sents(&total_bits_sent, &n_sent);
-			content += format("<TR><TD>get requests:</TD><TD>%d</TD></TR>\n", n_sent);
-			content += format("<TR><TD>bits requested:</TD><TD>%lld</TD></TR>\n", total_bits_sent);
-			content += format("<TR><TD>avg bits/get:</TD><TD>%f</TD></TR>\n", double(total_bits_sent) / double(n_sent));
-			content += format("<TR><TD>get bps:</TD><TD>%f</TD></TR>\n", double(total_bits_sent) / (now - pcs -> get_since_ts()));
+			content += format("<tr><td>get requests:</td><td>%d</td></tr>\n", n_sent);
+			content += format("<tr><td>bits requested:</td><td>%lld</td></tr>\n", total_bits_sent);
+			content += format("<tr><td>avg bits/get:</td><td>%f</td></tr>\n", double(total_bits_sent) / double(n_sent));
+			content += format("<tr><td>get bps:</td><td>%f</td></tr>\n", double(total_bits_sent) / (now - pcs -> get_since_ts()));
 
 			my_mutex_lock(&p -> stats_lck);
-			content += std::string("<TR><TD>FIPS140 stats:</TD><TD>") + p -> pfips140 -> stats() + "</TD></TR>\n";
-			content += std::string("<TR><TD>SCC stats:</TD><TD>") + p -> pscc -> stats() + "</TD></TR>\n";
+			content += std::string("<tr><td>FIPS140 stats:</td><td>") + p -> pfips140 -> stats() + "</td></tr>\n";
+			content += std::string("<tr><td>SCC stats:</td><td>") + p -> pscc -> stats() + "</td></tr>\n";
 			my_mutex_unlock(&p -> stats_lck);
 
-			content += "</TABLE>\n";
+			content += "</table>\n";
 		}
 
 		my_mutex_unlock(clients_mutex);
@@ -116,8 +116,8 @@ http_bundle * http_file_stats::do_request(http_request_t request_type, std::stri
 	else
 	{
 		// PER USER STATS
-		content += "<TABLE CLASS=\"table2\" WIDTH=\"100%\">\n";
-		content += "<TR CLASS=\"lighttable\"><TD>user</TD><TD>host</TD><TD>type</TD><TD>is server</TD><TD WIDTH=240>connected since</TD><TD>bits recv</TD><TD>bits sent</TD></TR>\n";
+		content += "<table class=\"table2\" WIDTH=\"100%\">\n";
+		content += "<TR class=\"lighttable\"><td>user</td><td>host</td><td>type</td><td>is server</td><td class=\"timestamp\">connected since</td><td>bits recv</td><td>bits sent</td></tr>\n";
 
 		double recv_bps = 0, sent_bps = 0;
 
@@ -126,17 +126,17 @@ http_bundle * http_file_stats::do_request(http_request_t request_type, std::stri
 		{
 			client_t *p = clients -> at(index);
 
-			content += "<TR><TD>";
+			content += "<tr><td>";
 			content += p -> username;
-			content += "</TD><TD><A HREF=\"stats.html?id=";
+			content += "</td><td><A HREF=\"stats.html?id=";
 			content += format("%d", p -> id);
 			content += "\">";
 			content += p -> host;
-			content += "</A></TD><TD>";
+			content += "</A></td><td>";
 			content += p -> type;
-			content += "</TD><TD>";
+			content += "</td><td>";
 			content += p -> is_server ? "yes" : "no";
-			content += "</TD><TD>";
+			content += "</td><td>";
 
 			statistics *pcs = p -> stats_user;
 
@@ -147,94 +147,94 @@ http_bundle * http_file_stats::do_request(http_request_t request_type, std::stri
 			int n_recv = 0, n_sent = 0;
 			pcs -> get_recvs(&total_bits_recv, &n_recv, &total_bits_recv_in);
 			pcs -> get_sents(&total_bits_sent, &n_sent);
-			content += "</TD><TD>";
+			content += "</td><td>";
 			content += format("%lld", total_bits_recv);
 			if (p -> is_server)
 				recv_bps += double(total_bits_recv) / duration;
 			else
 				sent_bps += double(total_bits_sent) / duration;
-			content += "</TD><TD>";
+			content += "</td><td>";
 			content += format("%lld", total_bits_sent);
-			content += "</TD></TR>\n";
+			content += "</td></tr>\n";
 		}
 		my_mutex_unlock(clients_mutex);
-		content += format("<TR CLASS=\"lighttable\"><TD COLSPAN=\"4\"></TD><TD ALIGN=\"right\">bps:</TD><TD>%.1f</TD><TD>%.1f</TD></TR>\n", recv_bps, sent_bps);
+		content += format("<TR class=\"lighttable\"><td COLSPAN=\"4\"></td><td ALIGN=\"right\">bps:</td><td>%.1f</td><td>%.1f</td></tr>\n", recv_bps, sent_bps);
 
-		content += "</TABLE>\n";
+		content += "</table>\n";
 		content += format("Number of connected clients/servers: %d<BR>\n", clients -> size());
 		content += "<BR>\n";
 
 		// GLOBAL STATS
-		content += "<TABLE CLASS=\"table2\" WIDTH=\"100%\">\n";
-		content += "<TR><TD WIDTH=200>running since:</TD><TD>" + time_to_str((time_t)get_start_ts()) + "</TD></TR>\n";
-		content += format("<TR><TD>duration:</TD><TD>%fs</TD></TR>\n", now - get_start_ts());
-		content += "<TR><TD>first msg:</TD><TD>" + time_to_str((time_t)ps -> get_since_ts()) + "</TD></TR>\n";
-		content += format("<TR><TD>avg time between msgs:</TD><TD>%fs</TD></TR>\n", (now - ps -> get_since_ts()) / double(ps -> get_msg_cnt()));
-		content += "<TR><TD>last message:</TD><TD>" + time_to_str((time_t)ps -> get_last_msg_ts()) + "</TD></TR>\n";
-		content += "<TR><TD>last put message:</TD><TD>" + time_to_str((time_t)ps -> get_last_put_msg_ts()) + "</TD></TR>\n";
+		content += "<table class=\"table2\" WIDTH=\"100%\">\n";
+		content += "<tr><td class=\"keys\">running since:</td><td>" + time_to_str((time_t)get_start_ts()) + "</td></tr>\n";
+		content += format("<tr><td>duration:</td><td>%fs</td></tr>\n", now - get_start_ts());
+		content += "<tr><td>first msg:</td><td>" + time_to_str((time_t)ps -> get_since_ts()) + "</td></tr>\n";
+		content += format("<tr><td>avg time between msgs:</td><td>%fs</td></tr>\n", (now - ps -> get_since_ts()) / double(ps -> get_msg_cnt()));
+		content += "<tr><td>last message:</td><td>" + time_to_str((time_t)ps -> get_last_msg_ts()) + "</td></tr>\n";
+		content += "<tr><td>last put message:</td><td>" + time_to_str((time_t)ps -> get_last_put_msg_ts()) + "</td></tr>\n";
 
-		content += format("<TR><TD>not allowed:</TD><TD>%d</TD></TR>\n", ps -> get_times_not_allowed());
-		content += format("<TR><TD>quota:</TD><TD>%d</TD></TR>\n", ps -> get_times_quota());
-		content += format("<TR><TD>pools empty:</TD><TD>%d</TD></TR>\n", ps -> get_times_empty());
-		content += format("<TR><TD>full:</TD><TD>%d</TD></TR>\n", ps -> get_times_full());
+		content += format("<tr><td>not allowed:</td><td>%d</td></tr>\n", ps -> get_times_not_allowed());
+		content += format("<tr><td>quota:</td><td>%d</td></tr>\n", ps -> get_times_quota());
+		content += format("<tr><td>pools empty:</td><td>%d</td></tr>\n", ps -> get_times_empty());
+		content += format("<tr><td>full:</td><td>%d</td></tr>\n", ps -> get_times_full());
 
 		long long int total_bits_recv = 0, total_bits_recv_in = 0;
 		int n_recv = 0;
 		ps -> get_recvs(&total_bits_recv, &n_recv, &total_bits_recv_in);
-		content += format("<TR><TD>put requests:</TD><TD>%d</TD></TR>\n", n_recv);
-		content += format("<TR><TD>bits put:</TD><TD>%lld</TD></TR>\n", total_bits_recv);
-		// FIXME should be get density content += format("<TR><TD>bits put density:</TD><TD>%f%%</TD></TR>\n", double(total_bits_recv * 100) / double(total_bits_recv_in));
-		content += format("<TR><TD>avg bits/put:</TD><TD>%f</TD></TR>\n", double(total_bits_recv) / double(n_recv));
-		content += format("<TR><TD>put bps:</TD><TD>%f</TD></TR>\n", double(total_bits_recv) / (now - ps -> get_since_ts()));
+		content += format("<tr><td>put requests:</td><td>%d</td></tr>\n", n_recv);
+		content += format("<tr><td>bits put:</td><td>%lld</td></tr>\n", total_bits_recv);
+		// FIXME should be get density content += format("<tr><td>bits put density:</td><td>%f%%</td></tr>\n", double(total_bits_recv * 100) / double(total_bits_recv_in));
+		content += format("<tr><td>avg bits/put:</td><td>%f</td></tr>\n", double(total_bits_recv) / double(n_recv));
+		content += format("<tr><td>put bps:</td><td>%f</td></tr>\n", double(total_bits_recv) / (now - ps -> get_since_ts()));
 
 		long long int total_bits_sent = 0;
 		int n_sent = 0;
 		ps -> get_sents(&total_bits_sent, &n_sent);
-		content += format("<TR><TD>get requests:</TD><TD>%d</TD></TR>\n", n_sent);
-		content += format("<TR><TD>bits requested:</TD><TD>%lld</TD></TR>\n", total_bits_sent);
-		content += format("<TR><TD>avg bits/get:</TD><TD>%f</TD></TR>\n", double(total_bits_sent) / double(n_sent));
-		content += format("<TR><TD>get bps:</TD><TD>%f</TD></TR>\n", double(total_bits_sent) / (now - ps -> get_since_ts()));
+		content += format("<tr><td>get requests:</td><td>%d</td></tr>\n", n_sent);
+		content += format("<tr><td>bits requested:</td><td>%lld</td></tr>\n", total_bits_sent);
+		content += format("<tr><td>avg bits/get:</td><td>%f</td></tr>\n", double(total_bits_sent) / double(n_sent));
+		content += format("<tr><td>get bps:</td><td>%f</td></tr>\n", double(total_bits_sent) / (now - ps -> get_since_ts()));
 
-		content += std::string("<TR><TD>FIPS140 stats:</TD><TD>") + pfips140 -> stats() + "</TD></TR>\n";
-		content += std::string("<TR><TD>SCC stats:</TD><TD>") + pscc -> stats() + "</TD></TR>\n";
+		content += std::string("<tr><td>FIPS140 stats:</td><td>") + pfips140 -> stats() + "</td></tr>\n";
+		content += std::string("<tr><td>SCC stats:</td><td>") + pscc -> stats() + "</td></tr>\n";
 
-		content += "</TABLE>\n";
+		content += "</table>\n";
 		content += "<BR>\n";
 
 		// POOL STATS
-		content += "<TABLE CLASS=\"table2\" WIDTH=100%>\n";
+		content += "<table class=\"table2\" WIDTH=100%>\n";
 		int bit_sum = ppools -> get_bit_sum(1.0);
-		content += format("<TR><TD WIDTH=200>in memory bit count:</TD><TD>%d</TD></TR>\n", bit_sum);
+		content += format("<tr><td class=\"keys\">in memory bit count:</td><td>%d</td></tr>\n", bit_sum);
 		int mem_pools = ppools -> get_memory_pool_count();
-		content += format("<TR><TD>pools in memory:</TD><TD>%d</TD></TR>\n", mem_pools);
-		content += format("<TR><TD>avg bits/mem pool:</TD><TD>%f (def max: %d)</TD></TR>\n", double(bit_sum) / double(mem_pools), DEFAULT_POOL_SIZE_BITS);
-		content += format("<TR><TD>pool files on disk:</TD><TD>%d</TD></TR>\n", ppools -> get_disk_pool_count());
-		content += "</TABLE>\n";
+		content += format("<tr><td>pools in memory:</td><td>%d</td></tr>\n", mem_pools);
+		content += format("<tr><td>avg bits/mem pool:</td><td>%f (def max: %d)</td></tr>\n", double(bit_sum) / double(mem_pools), DEFAULT_POOL_SIZE_BITS);
+		content += format("<tr><td>pool files on disk:</td><td>%d</td></tr>\n", ppools -> get_disk_pool_count());
+		content += "</table>\n";
 		content += "<BR>\n";
 
-		content += "<H2>number of memory pools</H2>\n";
+		content += "<h2>number of memory pools</h2>\n";
 		content += "<IMG SRC=\"/graph.png?type=mem_pool_counts&width=640&height=240\"><BR>\n";
 		content += "<BR>\n";
-		content += "<H2>number of disk pools</H2>\n";
+		content += "<h2>number of disk pools</h2>\n";
 		content += "<IMG SRC=\"/graph.png?type=dsk_pool_counts&width=640&height=240\"><BR>\n";
 		content += "<BR>\n";
-		content += "<H2>number of connections</H2>\n";
+		content += "<h2>number of connections</h2>\n";
 		content += "<IMG SRC=\"/graph.png?type=connection_counts&width=640&height=240\"><BR>\n";
 		content += "<BR>\n";
-		content += "<H2>memory pools bit count</H2>\n";
+		content += "<h2>memory pools bit count</h2>\n";
 		content += "<IMG SRC=\"/graph.png?type=mem_pools_bitcount&width=640&height=240\"><BR>\n";
 		content += "<BR>\n";
-		content += "<H2>disk pools bit count</H2>\n";
+		content += "<h2>disk pools bit count</h2>\n";
 		content += "<IMG SRC=\"/graph.png?type=dsk_pools_bitcount&width=640&height=240\"><BR>\n";
 		content += "<BR>\n";
 		content += format("Interval: %ds\n", MEASURE_INTERVAL);
-		content += "<H2>data from servers to broker (per interval, entropy count)</H2>\n";
+		content += "<h2>data from servers to broker (per interval, entropy count)</h2>\n";
 		content += "<IMG SRC=\"/graph.png?type=recv_bit_count&width=640&height=240\"><BR>\n";
 		content += "<BR>\n";
-		content += "<H2>data from servers to broker (per interval, raw data)</H2>\n";
+		content += "<h2>data from servers to broker (per interval, raw data)</h2>\n";
 		content += "<IMG SRC=\"/graph.png?type=recv_bit_count_in&width=640&height=240\"><BR>\n";
 		content += "<BR>\n";
-		content += "<H2>data from broker to clients (per interval)</H2>\n";
+		content += "<h2>data from broker to clients (per interval)</h2>\n";
 		content += "<IMG SRC=\"/graph.png?type=sent_bit_count&width=640&height=240\"><BR>\n";
 		content += "<BR>\n";
 	}
