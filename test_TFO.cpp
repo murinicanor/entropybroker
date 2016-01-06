1552806e021342c1dba2a37c065fa4cb7c50ddd4
Folkert van Heusden <folkert@vanheusden.com>
2016-01-06 12:26:23 +0100

    TFO availability check fix

--- test_TFO.cpp
+++ test_TFO.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <netinet/tcp.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
 int main(int argc, char *argv[])
 {
+	int fd = 0;
 	int qlen = 5;
 
-	setsockopt(sfd, SOL_TCP, TCP_FASTOPEN, &qlen, sizeof(qlen));
+	setsockopt(fd, SOL_TCP, TCP_FASTOPEN, &qlen, sizeof(qlen));
 
 	return 0;
 }
